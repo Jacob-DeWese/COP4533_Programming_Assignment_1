e8ba225ade8459ff9a36dc536c40bca9d38487df
Jacob-DeWese <dewese.jn@ufl.edu>
2026-01-26 13:41:32 -0500

    Removed Gale-Shapley header file (reconfiguring how to implement requirements)

--- Gale-Shapley.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by thear on 1/26/2026.
-//
-
-#ifndef GALE_SHAPLEY_H
-#define GALE_SHAPLEY_H
-
-#endif //GALE_SHAPLEY_H
--- main.cpp
+++ main.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+using namespace std;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
