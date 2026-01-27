f8a199fa5f3ae3b6626c2089a78b6d0839b6005b
Carson Hernandez <carson.hernandez@ufl.edu>
2026-01-27 18:31:58 -0500

    GaleShapely implementation
    GaleShapely class and header, along with some main changes

---
+++ cmake-build-debug/CMakeFiles/COP4533_Prog_Assi1.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for COP4533_Prog_Assi1.
+# This may be replaced when dependencies are built.
--- main.cpp
+++ main.cpp
@@ -1,25 +1,55 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <sstream>
 #include <vector>
 using namespace std;
-
+#include "GaleShapely.h"
 
 int main(int argc, char* argv[]) {
     if (argc < 3) {
-        cerr << "Usage: " << argv[0] << " <input_file> <output_file>" << endl;
+        cerr << "Usage: " << argv[0] << "<Matcher|Verifier> <input_file> <output_file>" << endl;
         return 1;
     }
 
     string instructionSelect = argv[1];
 
-    if (instructionSelect == "Matcher" || instructionSelect == "matcher" || instructionSelect == "Match" | instructionSelect == "match") {
+    if (instructionSelect == "Matcher" || instructionSelect == "matcher" || instructionSelect == "Match" || instructionSelect == "match") {
         ifstream inputFile;
         inputFile.open(argv[2]);
         if (!inputFile.is_open()) {
             cerr << "Error opening file " << argv[2] << endl;
             return 1;
+        }
+        int numPeople;
+        inputFile >> numPeople;
+        vector<vector<int>> hospitalPreferenceList(numPeople, vector<int>(numPeople));
+        vector<vector<int>> studentPreferenceList(numPeople, vector<int>(numPeople));
+        for(int i = 0; i < numPeople; i++){
+           string preference;
+           inputFile >> preference;
+           for(int j = 0; j < numPeople; j++){
+               hospitalPreferenceList[i][j] = preference[j] - '1';
+           }
+        }
+        for(int h = 0; h < numPeople; h++){
+            string preference;
+            inputFile >> preference;
+            for(int g = 0; g < numPeople; g++){
+                studentPreferenceList[h][g] = preference[g] - '1';
+            }
+        }
+        vector<vector<int>> studentRankingList(numPeople, vector<int>(numPeople));
+        for(int g = 0; g < numPeople; g++){
+           for(int h = 0; h < numPeople; h++){
+               int studentRanking = studentPreferenceList[g][h];
+               studentRankingList[g][studentRanking] = h;
+           }
+        }
+        inputFile.close();
+        GaleShapely galeShapely;
+        vector<vector<int>> matchings = galeShapely.GaleShapelyAlgorithm(hospitalPreferenceList, studentRankingList);
+        for(const auto& hospital : matchings){
+            cout << hospital[0] + 1 << " " << hospital[1] + 1<< endl;
         }
     }
     else if (instructionSelect == "Verifer" || instructionSelect == "verifier" || instructionSelect == "Verify" || instructionSelect == "verify") {
